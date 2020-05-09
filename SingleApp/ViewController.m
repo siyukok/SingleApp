1722a6ad824dc459889a804081cd74b9e02b9151
siyukok <kongzhi@ibantang.com>
2020-05-09 16:51:58 +0800

    reset

--- SingleApp/ViewController.m
+++ SingleApp/ViewController.m
@@ -7,34 +7,6 @@
 //
 
 #import "ViewController.h"
-@interface TestView : UIView
-@end
-
-@implementation TestView
-
-- (instancetype)init{
-    self = [super init];
-    if (self) {
-        
-    }
-    return self;
-}
-
-- (void)willMoveToSuperview:(nullable UIView *)newSuperview{
-    [super willMoveToSuperview:newSuperview];
-} 
-- (void)didMoveToSuperview{
-    [super didMoveToSuperview];
-}
-- (void)willMoveToWindow:(nullable UIWindow *)newWindow{
-    [super willMoveToWindow:newWindow];
-}
-- (void)didMoveToWindow{
-    [super didMoveToWindow];
-}
-
-@end
-
 @interface ViewController ()
 
 @end
@@ -42,25 +14,6 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-//    [self.view addSubview:({
-//        UILabel *label = [[UILabel alloc] init];
-//        label.text = @"hello world";
-//        [label sizeToFit];
-//        label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
-//        label;
-//    })];
-    
-    TestView *view1 = [[TestView alloc] init];
-    view1.backgroundColor = [UIColor redColor];
-    view1.frame = CGRectMake(100, 100, 100, 100);
-    [self.view addSubview:view1];
-    
-//    UIView *view2 = [[UIView alloc] init];
-//    view2.backgroundColor = [UIColor greenColor];
-//    view2.frame = CGRectMake(150, 150, 100, 100);
-//    [self.view addSubview:view2];
 }
 
 
